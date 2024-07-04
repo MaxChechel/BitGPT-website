8b9623eceb764ff31c407ddf0fff6a5a86501cf6
Max Chechel <maksymchechel@yahoo.com>
2024-07-04 16:35:34 +0200

    updated calculation in swap anim

--- src/mobile-animations/integrate/index.js
+++ src/mobile-animations/integrate/index.js
@@ -3,50 +3,65 @@ import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
 export default function integrateAnimation() {
   gsap.registerPlugin(MotionPathPlugin);
-  MotionPathPlugin.convertToPath(".integrate_orbit-circle");
-  const orbitWraps = document.querySelectorAll(
-    ".integrate_orbit-wrap:not(.is-outer)"
-  );
 
-  for (let i = 0; i < orbitWraps.length; i++) {
-    const circle = orbitWraps[i].querySelector(".integrate_orbit-circle");
-    const items = orbitWraps[i].querySelectorAll(".integrate_orbit-img");
-    items.forEach((item) => {
-      orbitWraps[i].appendChild(item);
-    });
-    //Set initial positions
-    gsap.set(items, {
-      motionPath: {
-        path: circle,
-        align: circle,
-        alignOrigin: [0.5, 0.5],
-        //autoRotate: true,
-      },
-    });
-    // Create a timeline to animate items along the path
-    const tl = gsap.timeline({
-      repeat: -1,
-      ease: "none",
-    });
+  const initializeAnimation = () => {
+    MotionPathPlugin.convertToPath(".integrate_orbit-circle");
+    const orbitWraps = document.querySelectorAll(
+      ".integrate_orbit-wrap:not(.is-outer)"
+    );
 
-    items.forEach((item, i) => {
-      tl.to(
-        item,
-        {
-          motionPath: {
-            path: circle,
-            align: circle,
-            alignOrigin: [0.5, 0.5],
-            //autoRotate: true,
-            start: i / items.length,
-            end: i / items.length + 1,
-            immediateRender: true,
-          },
-          duration: 40,
-          ease: "none",
+    orbitWraps.forEach((orbitWrap) => {
+      const circle = orbitWrap.querySelector(".integrate_orbit-circle");
+      const items = orbitWrap.querySelectorAll(".integrate_orbit-img");
+
+      // Clear any previous animation instances
+      gsap.killTweensOf(items);
+
+      items.forEach((item) => {
+        orbitWrap.appendChild(item);
+      });
+
+      // Set initial positions
+      gsap.set(items, {
+        motionPath: {
+          path: circle,
+          align: circle,
+          alignOrigin: [0.5, 0.5],
         },
-        0
-      );
+      });
+
+      // Create a timeline to animate items along the path
+      const tl = gsap.timeline({
+        repeat: -1,
+        ease: "none",
+      });
+
+      items.forEach((item, i) => {
+        tl.to(
+          item,
+          {
+            motionPath: {
+              path: circle,
+              align: circle,
+              alignOrigin: [0.5, 0.5],
+              start: i / items.length,
+              end: i / items.length + 1,
+              immediateRender: true,
+            },
+            duration: 40,
+            ease: "none",
+          },
+          0
+        );
+      });
     });
-  }
+  };
+
+  // Initialize animation on load
+  initializeAnimation();
+
+  // Reinitialize animation on window resize
+  window.addEventListener("resize", () => {
+    initializeAnimation();
+  });
 }
--- src/mobile-animations/swap/index.js
+++ src/mobile-animations/swap/index.js
@@ -2,105 +2,108 @@ import gsap from "gsap";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
 export default function swapAnimation() {
-  gsap.registerPlugin(MotionPathPlugin);
-  MotionPathPlugin.convertToPath(".orbit-circle, .pulse-path");
-  const orbitWraps = document.querySelectorAll(".swap_orbit-wrap");
-
-  for (let i = 0; i < orbitWraps.length; i++) {
-    const circle = orbitWraps[i].querySelector(".orbit-circle");
-    const items = orbitWraps[i].querySelectorAll(".swap_orbit-img");
-    items.forEach((item) => {
-      orbitWraps[i].appendChild(item);
+  function killAllAnimations() {
+    gsap.set(".swap_orbit-img, .orbit-circle, .swap_orbit-wrap, .pulse-line", {
+      clearProps: "all",
     });
+    gsap.killTweensOf(
+      ".swap_orbit-img, .orbit-circle, .swap_orbit-wrap, .pulse-line"
+    );
+  }
 
-    // Set initial positions of items
-    items.forEach((item, index) => {
-      const position = index / items.length;
-      gsap.set(item, {
-        motionPath: {
-          path: circle,
-          align: circle,
-          alignOrigin: [0.5, 0.5],
-          start: position,
-          end: position + 1 / items.length,
-          autoRotate: false,
-        },
+  function initializeAnimations() {
+    gsap.registerPlugin(MotionPathPlugin);
+    MotionPathPlugin.convertToPath(".orbit-circle, .pulse-path");
+    const orbitWraps = document.querySelectorAll(".swap_orbit-wrap");
+    orbitWraps.forEach((orbitWrap, i) => {
+      const circle = orbitWrap.querySelector(".orbit-circle");
+      const items = orbitWrap.querySelectorAll(".swap_orbit-img");
+      items.forEach((item) => {
+        orbitWrap.appendChild(item);
       });
-    });
 
-    // function rotate() {
-    //   gsap.to(orbitWraps[i], {
-    //     rotation: `-=${360 / items.length}`,
-    //     transformOrigin: "center",
-    //     duration: 2,
-    //     ease: "none",
-    //   });
-    //   //   gsap.to(items, {
-    //   //     rotation: `-=180`,
-    //   //     transformOrigin: "center",
-    //   //     duration: 2,
-    //   //     ease: "none",
-    //   //   });
-    //   rotation = gsap.delayedCall(3, rotate);
-    // }
+      items.forEach((item, index) => {
+        const position = index / items.length;
+        gsap.set(item, {
+          motionPath: {
+            path: circle,
+            align: circle,
+            alignOrigin: [0.5, 0.5],
+            start: position,
+            end: position + 1 / items.length,
+            autoRotate: false,
+          },
+        });
+      });
 
-    // rotation = gsap.delayedCall(1, rotate);
-    setTimeout(() => {
       const tl = gsap.timeline({
+        delay: 1.5,
         defaults: {
           duration: 1.5,
           ease: "none",
-          rotation: "+=90",
           transformOrigin: "center",
           repeat: -1,
           repeatRefresh: true,
           repeatDelay: 3,
         },
       });
-      tl.to(orbitWraps[i], {});
+      console.log(`+=${360 / items.length}`);
+      tl.to(orbitWrap, {
+        rotation: `+=${360 / items.length}`,
+      });
       tl.to(
         items,
-        { duration: 1.5, rotation: "-=90", transformOrigin: "center center" },
+        {
+          duration: 1.5,
+          rotation: `-=${360 / items.length}`,
+          transformOrigin: "center center",
+        },
         0
       );
-    }, 1500);
+
+      const pathSvgs = document.querySelectorAll(".pulse-group");
+
+      pathSvgs.forEach((svg) => {
+        const path = svg.querySelector(".pulse-path");
+        const line = svg.querySelector(".pulse-line");
+        const timeToPlay = 1.5;
+        const timeDelay = 3;
+
+        gsap.set(line, {
+          xPercent: -50,
+          yPercent: -50,
+          transformOrigin: "50% 50%",
+        });
+        gsap.to(".pulse-line", {
+          opacity: 1,
+          duration: 0,
+        });
+        const tween = gsap.timeline().to(line, {
+          motionPath: {
+            path: path,
+            align: path,
+            autoRotate: true,
+            alignOrigin: [0.5, 0.5],
+            start: 1,
+            end: 0,
+          },
+          delay: 0,
+          duration: timeToPlay,
+          repeat: -1,
+          repeatDelay: timeDelay,
+          ease: "none",
+          opacity: 0,
+        });
+
+        tween.play();
+      });
+    });
   }
 
-  const pathSvgs = document.querySelectorAll(".pulse-group");
+  initializeAnimations();
 
-  pathSvgs.forEach((svg, i) => {
-    const path = svg.querySelector(".pulse-path");
-    const line = svg.querySelector(".pulse-line");
-    const timeToPlay = 1.5;
-    const timeDelay = 3;
-
-    // Set the transform origin and offsets before animating
-    gsap.set(line, {
-      xPercent: -50, // Center horizontally
-      yPercent: -50, // Center vertically
-      transformOrigin: "50% 50%", // Set the transform origin to the center
-    });
-    gsap.to(".pulse-line", {
-      opacity: 1,
-      duration: 0,
-    });
-    const tween = gsap.to(line, {
-      motionPath: {
-        path: path,
-        align: path,
-        autoRotate: true,
-        alignOrigin: [0.5, 0.5],
-        start: 1,
-        end: 0,
-      },
-      delay: 0,
-      duration: timeToPlay,
-      repeat: -1,
-      repeatDelay: timeDelay,
-      ease: "none",
-      opacity: 0,
-    });
-
-    tween.play();
+  window.addEventListener("resize", () => {
+    killAllAnimations();
+    initializeAnimations();
   });
 }
