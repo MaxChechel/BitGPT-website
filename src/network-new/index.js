9850505eccf8ca99aff396ef0ecc0f9310d979e7
Max Chechel <maksymchechel@yahoo.com>
2024-10-26 00:17:18 +0200

    bundled scripts

--- src/network-new/index.js
+++ src/network-new/index.js
@@ -15,33 +15,7 @@ gsap.registerPlugin(ScrollTrigger);
 //  Circuit animation for CTA section
 circuitBoardAnimation(".section-cta-bg_img .circuit-lines-group path");
 //////Hero loader
-document.fonts.ready.then(function () {
-  // const heroParagraph = new SplitType(".network-hero_content-wrap p");
-  // gsap.set(".network-hero_content-wrap p", { opacity: 1 });
-  // const heroTl = gsap.timeline();
-  // heroTl
-  //   .to(".hero-heading .heading-span-line", {
-  //     delay: 0.2,
-  //     duration: 0.5,
-  //     ease: "circ.out",
-  //     y: "0%",
-  //     opacity: 1,
-  //     stagger: { each: 0.025 },
-  //     transformOrigin: "center bottom",
-  //     rotationX: 0,
-  //   })
-  //   .to(
-  //     ".network-hero_content-wrap p .line",
-  //     {
-  //       duration: 0.6,
-  //       ease: "circ.out",
-  //       y: "0%",
-  //       opacity: 1,
-  //       stagger: { each: 0.035 },
-  //     },
-  //     "<15%"
-  //   );
-});
+document.fonts.ready.then(function () {});
 
 const stickySections = document.querySelectorAll(".network-sticky_section");
 const anchors = document.querySelectorAll(
@@ -69,16 +43,7 @@ const ctaCircuitTimeline = circuitBoardAnimation(
   ".section-cta-bg_img .circuit-lines-group path"
 );
 ctaCircuitTimeline.play();
-// ScrollTrigger.create({
-//   trigger: ".section_network-features",
-//   start: "bottom 100%",
-//   end: "bottom 0%",
-//   invalidateOnRefresh: true,
-//   onEnter: () => ctaCircuitTimeline.play(),
-//   onLeave: () => ctaCircuitTimeline.pause(),
-//   onEnterBack: () => ctaCircuitTimeline.play(),
-//   onLeaveBack: () => ctaCircuitTimeline.pause(),
-// });
+
 ScrollTrigger.create({
   trigger: ".cta_card",
   start: "top 60%",
